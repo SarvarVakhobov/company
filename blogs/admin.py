5efcea04d6e350359fdab93ab85f8ac1325e78bd
Sarvarbek <sarvarbek0125@gmail.com>
2022-03-15 17:34:26 +0500

    main

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,8 @@
 from unicodedata import category
+from .forms import *
 from django.contrib import admin
 from .models import Category, Blog, Comments
+from ckeditor.widgets import CKEditorWidget
 
 # Register your models here.
 admin.site.register(Category)
@@ -9,3 +11,7 @@ admin.site.register(Comments)
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'date']
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = Blog
+        fields = '__all__'
